08c410bcd8bfed5422621ee7a45abddc320b92ea
Mamadou Cisse <mciissee@gmail.com>
2023-02-01 21:17:34 +0100

    feat(api): setup validators

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,17 +3,33 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, LogLevel, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+const LOG_LEVELS: LogLevel[] =
+  process.env.NODE_ENV === 'development'
+    ? ['debug', 'error', 'log', 'verbose', 'warn']
+    : ['error', 'warn']
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: LOG_LEVELS,
+  });
+
+
+  const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
+
+
+  app.useLogger(app.get(Logger))
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }))
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
