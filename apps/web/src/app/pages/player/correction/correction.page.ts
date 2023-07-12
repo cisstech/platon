e8a540a45a66a304118e7f885168760b77520770
clementg <derkeethus96@gmail.com>
2023-07-12 15:37:43 +0200

    refactor(module:web): fix linter warnings

--- apps/web/src/app/pages/player/correction/correction.page.ts
+++ apps/web/src/app/pages/player/correction/correction.page.ts
@@ -1,17 +1,12 @@
 import { CommonModule } from '@angular/common'
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-
-import { MatCardModule } from '@angular/material/card'
-
 import { NzSpinModule } from 'ng-zorro-antd/spin'
-
 import { PlayerCorrectionComponent } from '@platon/feature/player/browser'
 import { Player } from '@platon/feature/player/common'
 import { ResultService } from '@platon/feature/result/browser'
 import { PendingCorrection } from '@platon/feature/result/common'
 import { UiErrorComponent } from '@platon/shared/ui'
-import { NzSkeletonModule } from 'ng-zorro-antd/skeleton'
 import { firstValueFrom } from 'rxjs'
 
 @Component({
