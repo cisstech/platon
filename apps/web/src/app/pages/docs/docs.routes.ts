15050d0fcd32adcfdf99c4f42d06496e42ba7fd7
clementg <derkeethus96@gmail.com>
2023-07-18 14:33:11 +0200

    feature(module:docs): add page for teacher doc and github links to edit them

--- apps/web/src/app/pages/docs/docs.routes.ts
+++ apps/web/src/app/pages/docs/docs.routes.ts
@@ -14,7 +14,43 @@ const DeveloperDocs = {
       url: 'https://github.com/PlatonOrg/platon',
     },
   },
-  pages: [{ title: 'Présentation', href: 'presentation', renderer: 'assets/docs/developers/index.md' }],
+  pages: [
+    { title: 'Présentation',
+    href: 'presentation',
+    renderer: 'assets/docs/developers/index.md' ,
+    actions: [
+      {
+        title: 'Éditer sur Github',
+        icon: 'https://icongr.am/octicons/mark-github.svg',
+        run: `https://github.com/PlatonOrg/platon/blob/main/shared/assets/docs/developers/index.md`,
+      }]
+  }
+  ],
+} as NgeDocSettings
+
+const TeacherDocs = {
+  meta: {
+    name: 'PLaTon pour les enseignants',
+    root: '/docs/teachers/',
+    logo: 'assets/images/logo/platon.svg',
+    backUrl: '/docs',
+    repo: {
+      name: 'platon',
+      url: 'https://github.com/PlatonOrg/platon',
+    },
+  },
+  pages: [
+    { title: 'Présentation',
+    href: 'presentation',
+    renderer: 'assets/docs/teachers/index.md' ,
+    actions: [
+      {
+        title: 'Éditer sur Github',
+        icon: 'https://icongr.am/octicons/mark-github.svg',
+        run: `https://github.com/PlatonOrg/platon/blob/main/shared/assets/docs/developers/teachers.md`,
+      }]
+  }
+  ],
 } as NgeDocSettings
 
 export default [
@@ -27,6 +63,7 @@ export default [
     loadChildren: () => import('@cisstech/nge/doc').then((m) => m.NgeDocModule),
     data: [
       DeveloperDocs,
+      TeacherDocs,
       createWebComponentDoc({
         root: '/docs/pages/',
         backUrl: '/docs',
---
+++ shared/assets/docs/teachers/index.md
@@ -0,0 +1,3 @@
+# PLaTon enseignants
+
+TODO
