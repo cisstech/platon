8d5a74abb7c694af574553b0778a930c9067cc69
Mamadou Cisse <mciissee@gmail.com>
2023-11-25 17:26:43 +0100

    feat(editor): preview in new tab

--- apps/web/src/app/pages/editor/contributions/previews/pl-preview.contribution.ts
+++ apps/web/src/app/pages/editor/contributions/previews/pl-preview.contribution.ts
@@ -1,12 +1,16 @@
 import { Injectable, Injector, NgModule } from '@angular/core'
 import {
   CONTRIBUTION,
+  EditorService,
+  ICommand,
   IContribution,
   Preview,
   PreviewHandler,
   PreviewService,
   PreviewTypes,
 } from '@cisstech/nge-ide/core'
+import { CodIcon } from '@cisstech/nge/ui/icon'
+import { PLAYER_FULLSCREEN } from '@platon/feature/player/browser'
 import { Subscription } from 'rxjs'
 import { EditorPresenter } from '../../editor.presenter'
 
@@ -16,23 +20,53 @@ export class Contribution implements IContribution {
   readonly id = 'platon.contrib.preview'
 
   activate(injector: Injector) {
+    const editorService = injector.get(EditorService)
     const previewService = injector.get(PreviewService)
     const presenter = injector.get(EditorPresenter)
+
+    const canPreviewUri = (uri: monaco.Uri) => {
+      const { owner } = presenter.findOwnerResource(uri)
+      if (!owner || owner.type === 'CIRCLE') return false
+      return uri.path === '/main.ple' || uri.path === '/main.pla'
+    }
+
+    const buildPreviewUrl = (uri: monaco.Uri, params?: string[]) => {
+      const [resource, version] = uri.authority.split(':')
+      const queryParams = params ? '&' + params?.join('&') : ''
+      return `/player/preview/${resource}?version=${version}${queryParams}`
+    }
+
     previewService.register(
       new (class implements PreviewHandler {
         private counter = 0 // temporary solution to trigger change detection of the preview editor
-        canHandle(uri: monaco.Uri) {
-          const { owner } = presenter.findOwnerResource(uri)
-          if (!owner || owner.type === 'CIRCLE') return false
-          return uri.path === '/main.ple' || uri.path === '/main.pla'
-        }
+        canHandle = canPreviewUri
 
         async handle(_: Injector, uri: monaco.Uri): Promise<Preview> {
-          const [resource, version] = uri.authority.split(':')
           return Promise.resolve({
             type: PreviewTypes.URL,
-            data: `/player/preview/${resource}?version=${version}&counter=${++this.counter}`,
+            data: buildPreviewUrl(uri, [`counter=${++this.counter}`, PLAYER_FULLSCREEN]),
           })
+        }
+      })()
+    )
+
+    editorService.registerCommands(
+      new (class implements ICommand {
+        readonly id = 'platon.contrib.ple.commands.preview-in-new-tab'
+        readonly label = 'Prévisualiser dans un nouvel onglet'
+        readonly icon = new CodIcon('browser')
+
+        get enabled(): boolean {
+          const { activeResource } = editorService
+          if (!activeResource) return false
+          return canPreviewUri(activeResource)
+        }
+
+        async execute(): Promise<void> {
+          const { activeResource } = editorService
+          if (!activeResource) return
+
+          window.open(buildPreviewUrl(activeResource), '_blank')
         }
       })()
     )
