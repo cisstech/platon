9c9120f87031512334d2439babbafcda51bb994c
Mamadou Cisse <mciissee@gmail.com>
2023-12-12 09:58:45 +0100

    chore: setup new doc reverse proxy

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -6,15 +6,22 @@ const withNextra = require('nextra')({
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  //output: 'export',
+const nextConfig = process.env.NODE_ENV === 'production' ?  {
+  output: 'export',
   basePath: '/docs/main',
   assetPrefix: '/docs/main/',
   poweredByHeader: true,
   images: {
     unoptimized: true,
   },
- // distDir: '../../dist/apps/docs'
+  distDir: '../../dist/apps/docs'
+} : {
+  basePath: '/docs/main',
+  assetPrefix: '/docs/main/',
+  poweredByHeader: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = withNextra(nextConfig)
