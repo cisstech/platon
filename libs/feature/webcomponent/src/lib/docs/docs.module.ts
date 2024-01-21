9dc552e8eca403c4db582670a1f3551380529fdd
Mamadou Cisse <mciissee@gmail.com>
2024-01-21 16:02:29 +0100

    refactor(webcomponent): rework api displaying style

--- libs/feature/webcomponent/src/lib/docs/docs.module.ts
+++ libs/feature/webcomponent/src/lib/docs/docs.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { DocsComponent } from './docs.component'
-import { ShowcaseComponent } from './showcase/showcase.component'
-import { NgeMarkdownModule } from '@cisstech/nge/markdown'
-import { NgJsonEditorModule } from 'ang-jsoneditor'
+import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
+import { NgeMarkdownModule } from '@cisstech/nge/markdown'
 import { IDynamicModule } from '@cisstech/nge/services'
+import { NgJsonEditorModule } from 'ang-jsoneditor'
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
+import { DocsComponent } from './docs.component'
+import { JSONSchemaExpandablePipe } from './pipes/json-schema-expandable.pipe'
+import { JSONSchemaTypeNamePipe } from './pipes/json-schema-type-name.pipe'
+import { PropertiesComponent } from './properties/properties.component'
+import { ShowcaseComponent } from './showcase/showcase.component'
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, NzToolTipModule, NgeMarkdownModule, NgJsonEditorModule],
-  declarations: [DocsComponent, ShowcaseComponent],
+  declarations: [
+    DocsComponent,
+    JSONSchemaExpandablePipe,
+    JSONSchemaTypeNamePipe,
+    PropertiesComponent,
+    ShowcaseComponent,
+  ],
 })
 export class DocsModule implements IDynamicModule {
   component = DocsComponent
