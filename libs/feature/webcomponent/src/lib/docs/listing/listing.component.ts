d2ddae89b6aa8d7f253b9a133ee9098bdc53fcf4
Mamadou Cisse <mciissee@gmail.com>
2024-01-31 23:19:52 +0100

    fix(webcomponent): presentation example not working

--- libs/feature/webcomponent/src/lib/docs/listing/listing.component.ts
+++ libs/feature/webcomponent/src/lib/docs/listing/listing.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common'
 import { Component, OnInit } from '@angular/core'
+import { MatCardModule } from '@angular/material/card'
+import { RouterModule } from '@angular/router'
 import { WebComponentDefinition, WebComponentTypes } from '../../web-component'
 import { WebComponentService } from '../../web-component.service'
 
 @Component({
+  standalone: true,
   selector: 'wc-docs-listing',
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.scss'],
+  imports: [CommonModule, RouterModule, MatCardModule],
 })
 export class ListingComponent implements OnInit {
   type!: WebComponentTypes
