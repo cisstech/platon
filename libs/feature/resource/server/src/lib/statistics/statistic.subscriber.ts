5d45a53b54a996064e2fe35d09c746686293e1d9
Mamadou Cisse <mciissee@gmail.com>
2024-02-05 22:00:32 +0100

    feat(resource): set resource update date on update file

--- libs/feature/resource/server/src/lib/statistics/statistic.subscriber.ts
+++ libs/feature/resource/server/src/lib/statistics/statistic.subscriber.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
+import { OnEvent } from '@nestjs/event-emitter'
 import { DataSource, EntitySubscriberInterface, InsertEvent, RemoveEvent, UpdateEvent } from 'typeorm'
+import { ON_CHANGE_FILE_EVENT, OnChangeFileEventPayload } from '../files/file.event'
 
 const TABLES = ['Resources', 'ResourceMembers', 'ResourceWatchers']
 
 @Injectable()
 export class ResourceStatsSubscriber implements EntitySubscriberInterface {
+  private readonly logger = new Logger(ResourceStatsSubscriber.name)
   constructor(private readonly dataSource: DataSource) {
     this.dataSource.subscribers.push(this)
   }
@@ -23,7 +26,26 @@ export class ResourceStatsSubscriber implements EntitySubscriberInterface {
 
   private async onEvent(event: InsertEvent<unknown> | UpdateEvent<unknown> | RemoveEvent<unknown>): Promise<void> {
     if (TABLES.includes(event.metadata.tableName)) {
+      console.log('Refreshing materialized view')
       await event.manager.query(`REFRESH MATERIALIZED VIEW "ResourceStats"`)
+    }
+  }
+
+  @OnEvent(ON_CHANGE_FILE_EVENT)
+  protected async onChangeFile(payload: OnChangeFileEventPayload): Promise<void> {
+    try {
+      this.logger.log('Handling ON_CHANGE_FILE_EVENT: Refreshing stats materialized view')
+      await this.dataSource.query(
+        `
+        UPDATE "Resources"
+        SET updated_at = NOW()
+        WHERE id = $1
+      `,
+        [payload.resource.id]
+      )
+      await this.dataSource.query(`REFRESH MATERIALIZED VIEW "ResourceStats"`)
+    } catch (error) {
+      this.logger.error('Error handling ON_CHANGE_FILE_EVENT', error)
     }
   }
 }
