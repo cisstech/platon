74cb1bcb6b0181c3bad81d916d909f9f9530afc3
Thomas Saillard <thomas.saillard@protonmail.com>
2024-04-05 17:03:52 +0200

    refactor(discord): removing useless code

--- libs/feature/discord/server/src/index.ts
+++ libs/feature/discord/server/src/index.ts
@@ -1,3 +1,2 @@
-export * from './lib/feature-discord-server.gateway'
 export * from './lib/feature-discord-server.service'
 export * from './lib/feature-discord-server.module'
--- libs/feature/discord/server/src/lib/feature-discord-server.module.ts
+++ libs/feature/discord/server/src/lib/feature-discord-server.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { DiscordModule } from '@discord-nestjs/core';
-import { FeatureDiscordServerGateway } from './feature-discord-server.gateway';
 import { LeaderboardCommand } from './commands/leaderboard.command';
 import { FeatureResultServerModule } from '@platon/feature/result/server';
 import { UnLeaderboardCommand } from './commands/unleaderboard.command';
@@ -8,6 +7,6 @@ import { UnLeaderboardCommand } from './commands/unleaderboard.command';
 
 @Module({
   imports: [DiscordModule.forFeature(), FeatureResultServerModule],
-  providers: [FeatureDiscordServerGateway, LeaderboardCommand, UnLeaderboardCommand]
+  providers: [LeaderboardCommand, UnLeaderboardCommand]
 })
 export class FeatureDiscordServerModule {}
