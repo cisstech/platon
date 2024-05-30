3b42b01d3de24f9b61708931ef69be059d595840
Valber <valber.contact@gmail.com>
2024-05-30 12:35:31 +0200

    style(correction-web): display a loading spinner when corrections are loading

--- apps/web/src/app/pages/corrections/pendings/pendings.page.ts
+++ apps/web/src/app/pages/corrections/pendings/pendings.page.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, inject }
 import { CorrectionTableComponent, ResultService } from '@platon/feature/result/browser'
 import { ActivityCorrection } from '@platon/feature/result/common'
 import { NzEmptyModule } from 'ng-zorro-antd/empty'
+import { NzSpinModule } from 'ng-zorro-antd/spin'
 import { firstValueFrom } from 'rxjs'
 
 @Component({
@@ -11,17 +12,19 @@ import { firstValueFrom } from 'rxjs'
   templateUrl: './pendings.page.html',
   styleUrls: ['./pendings.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, NzEmptyModule, CorrectionTableComponent],
+  imports: [CommonModule, NzEmptyModule, CorrectionTableComponent, NzSpinModule],
 })
 export class CorrectionsPendingsPage implements OnInit {
   private readonly resultService = inject(ResultService)
   private readonly changeDetectorRef = inject(ChangeDetectorRef)
 
   protected corrections: ActivityCorrection[] = []
+  protected loading = true
 
   async ngOnInit(): Promise<void> {
     const response = await firstValueFrom(this.resultService.listPendingCorrections())
     this.corrections = response.resources
     this.changeDetectorRef.markForCheck()
+    this.loading = false
   }
 }
