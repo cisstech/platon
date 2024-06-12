9a312f72da77bad754062b5e1d24e657a009f542
codefactor-io <support@codefactor.io>
2024-06-12 21:43:58 +0000

    [CodeFactor] Apply fixes

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -18,23 +18,23 @@ const nextConfig = process.env.GITHUB_PAGES
       distDir: '../../dist/apps/home/docs/main',
     }
   : process.env.NODE_ENV === 'production'
-  ? {
-      output: 'export',
-      basePath: '/docs/main',
-      assetPrefix: '/docs/main/',
-      poweredByHeader: true,
-      images: {
-        unoptimized: true,
-      },
-      distDir: '../../dist/apps/docs',
-    }
-  : {
-      basePath: '/docs/main',
-      assetPrefix: '/docs/main/',
-      poweredByHeader: true,
-      images: {
-        unoptimized: true,
-      },
-    }
+    ? {
+        output: 'export',
+        basePath: '/docs/main',
+        assetPrefix: '/docs/main/',
+        poweredByHeader: true,
+        images: {
+          unoptimized: true,
+        },
+        distDir: '../../dist/apps/docs',
+      }
+    : {
+        basePath: '/docs/main',
+        assetPrefix: '/docs/main/',
+        poweredByHeader: true,
+        images: {
+          unoptimized: true,
+        },
+      }
 
 module.exports = withNextra(nextConfig)
