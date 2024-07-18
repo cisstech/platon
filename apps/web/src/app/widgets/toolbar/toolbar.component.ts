0730eda18309d4b5a7d9da08ddad4589c3599e1c
Valber <valber.contact@gmail.com>
2024-07-18 12:18:22 +0200

    feat(toolbar): Can now access our personnal circle in the avatar menu

--- apps/web/src/app/widgets/toolbar/toolbar.component.ts
+++ apps/web/src/app/widgets/toolbar/toolbar.component.ts
@@ -25,7 +25,7 @@ import { NzBadgeModule } from 'ng-zorro-antd/badge'
 import { NzButtonModule } from 'ng-zorro-antd/button'
 import { NzIconModule } from 'ng-zorro-antd/icon'
 import { NzPopoverModule } from 'ng-zorro-antd/popover'
-import { Subscription } from 'rxjs'
+import { firstValueFrom, Subscription } from 'rxjs'
 
 @Component({
   standalone: true,
@@ -65,6 +65,7 @@ export class ToolbarComponent implements OnInit, OnDestroy {
   private readonly subscriptions: Subscription[] = []
 
   protected user?: User | undefined
+  protected personalCircleId?: string | undefined
 
   protected canCreateCourse = false
   protected canCreateCircle = false
@@ -101,6 +102,7 @@ export class ToolbarComponent implements OnInit, OnDestroy {
     this.drawerOpenedChange.emit(this.drawerOpened)
 
     this.user = (await this.authService.ready()) as User
+    this.personalCircleId = (await firstValueFrom(this.resourceService.circle(this.user.username))).id
 
     this.canCreateCourse = this.user.role === UserRoles.admin || this.user.role === UserRoles.teacher
 
