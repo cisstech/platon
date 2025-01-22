668417e687daba13efd32e2f61d4b21141022a3f
Nomehh <mathperriot@gmail.com>
2025-01-22 15:15:08 +0100

    fix(KCile): remove debug log

--- libs/feature/result/browser/src/components/result-box-plot/result-box-plot.component.ts
+++ libs/feature/result/browser/src/components/result-box-plot/result-box-plot.component.ts
@@ -76,10 +76,6 @@ export class ResultBoxPlotComponent implements AfterViewInit {
       return [min, Q1, median, Q3, max]
     })
 
-    console.error('data', data)
-
-    console.error('preparedData', preparedData)
-
     const option = {
       title: {
         text: 'Diagramme à moustaches',
@@ -100,7 +96,7 @@ export class ResultBoxPlotComponent implements AfterViewInit {
           show: true,
         },
         axisLabel: {
-          rotate: 45,
+          rotate: 25,
           fontSize: 10,
           interval: 0,
           formatter: (value: string) => (value.length > 17 ? value.slice(0, 17) + '...' : value),
@@ -109,6 +105,8 @@ export class ResultBoxPlotComponent implements AfterViewInit {
       yAxis: {
         type: 'value',
         name: 'Score',
+        min: -1,
+        max: 100,
         splitArea: {
           show: true,
         },
