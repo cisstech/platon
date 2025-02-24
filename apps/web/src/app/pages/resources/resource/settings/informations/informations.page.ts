64e7d67b07aa3514de82150b8316127b34fb001d
Tekpa <tekpamax97@gmail.com>
2025-02-24 15:34:23 +0100

    feat(ResourceInformationsPage) :  Added NzModalService for popconfirm

--- apps/web/src/app/pages/resources/resource/settings/informations/informations.page.ts
+++ apps/web/src/app/pages/resources/resource/settings/informations/informations.page.ts
@@ -16,6 +16,7 @@ import { Level, Topic } from '@platon/core/common'
 import { Subscription, firstValueFrom } from 'rxjs'
 import { ResourcePresenter } from '../../resource.presenter'
 import { TagService } from '@platon/core/browser'
+import { NzModalService } from 'ng-zorro-antd/modal'
 
 @Component({
   standalone: true,
@@ -37,12 +38,15 @@ import { TagService } from '@platon/core/browser'
     NzButtonModule,
     NzSelectModule,
   ],
+  providers: [NzModalService],
 })
 export class ResourceInformationsPage implements OnInit, OnDestroy {
   private readonly subscriptions: Subscription[] = []
   private readonly presenter = inject(ResourcePresenter)
   private readonly changeDetectorRef = inject(ChangeDetectorRef)
   private readonly tagService = inject(TagService)
+
+  constructor(private modal: NzModalService) {}
 
   protected dataSource?: DataSource
   protected listOfTagOptions: string[] = []
@@ -69,12 +73,77 @@ export class ResourceInformationsPage implements OnInit, OnDestroy {
     return /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(input)
   }
 
+  private confirmAddTopic(sujet: string, name: string, originalName: string): Promise<'cancel' | 'force' | 'similar'> {
+    return new Promise((resolve) => {
+      const modalRef = this.modal.create({
+        nzTitle: `${sujet} similaire trouvé`,
+        nzContent: `Le ${sujet} "${name}" est similaire à un autre intitulé "${originalName}". Que voulez-vous faire ?`,
+        nzClosable: false,
+        nzCancelText: null,
+        nzOkText: null,
+        nzFooter: [
+          {
+            label: "Prendre l'intitulé similaire",
+            type: 'default',
+            onClick: () => {
+              modalRef.destroy()
+              resolve('similar')
+            },
+          },
+          {
+            label: "Forcer l'ajout",
+            type: 'primary',
+            danger: true,
+            onClick: () => {
+              modalRef.destroy()
+              resolve('force')
+            },
+          },
+          {
+            label: 'Annuler',
+            type: 'default',
+            onClick: () => {
+              modalRef.destroy()
+              resolve('cancel')
+            },
+          },
+        ],
+      })
+    })
+  }
+
   protected async onNewLevels(levels: string[]): Promise<void> {
     for (const l of levels) {
       if (!this.isUUID4(l)) {
-        const level = await firstValueFrom(this.tagService.createLevel({ name: l }))
-        levels[levels.indexOf(l)] = level.id
-        this.dataSource?.levels.push(level)
+        try {
+          const level = await firstValueFrom(this.tagService.createLevel({ name: l }))
+          if (level.existing) {
+            // If level already exists, ask user what to do
+            const action = await this.confirmAddTopic('Niveau', l, level.name)
+
+            switch (action) {
+              case 'force': {
+                const forcedLevel = await firstValueFrom(this.tagService.createLevel({ name: l, force: true }))
+                levels[levels.indexOf(l)] = forcedLevel.id
+                this.dataSource?.levels.push(forcedLevel)
+                break
+              }
+
+              case 'similar':
+                levels[levels.indexOf(l)] = level.id
+                this.dataSource?.levels.push(level)
+                break
+
+              case 'cancel':
+                break
+            }
+          } else {
+            levels[levels.indexOf(l)] = level.id
+            this.dataSource?.levels.push(level)
+          }
+        } catch (error) {
+          console.error('Erreur lors de la création du niveau:', error)
+        }
       }
     }
   }
@@ -82,9 +151,36 @@ export class ResourceInformationsPage implements OnInit, OnDestroy {
   protected async onNewTopics(topics: string[]): Promise<void> {
     for (const t of topics) {
       if (!this.isUUID4(t)) {
-        const topic = await firstValueFrom(this.tagService.createTopic({ name: t }))
-        topics[topics.indexOf(t)] = topic.id
-        this.dataSource?.topics.push(topic)
+        try {
+          const topic = await firstValueFrom(this.tagService.createTopic({ name: t }))
+
+          if (topic.existing) {
+            // If topic already exists, ask user what to do
+            const action = await this.confirmAddTopic('Topic', t, topic.name)
+
+            switch (action) {
+              case 'force': {
+                const forcedTopic = await firstValueFrom(this.tagService.createTopic({ name: t, force: true }))
+                topics[topics.indexOf(t)] = forcedTopic.id
+                this.dataSource?.topics.push(forcedTopic)
+                break
+              }
+
+              case 'similar':
+                topics[topics.indexOf(t)] = topic.id
+                this.dataSource?.topics.push(topic)
+                break
+
+              case 'cancel':
+                break
+            }
+          } else {
+            topics[topics.indexOf(t)] = topic.id
+            this.dataSource?.topics.push(topic)
+          }
+        } catch (error) {
+          console.error('Erreur lors de la création du topic:', error)
+        }
       }
     }
   }
