8ef451055fa910566159ef6ba5117a64b87d5453
Tekpa <tekpamax97@gmail.com>
2025-02-25 12:41:31 +0100

    feat(AdminTagsPage) : checking similar tags #223

--- apps/web/src/app/pages/admin/tags/tags.page.ts
+++ apps/web/src/app/pages/admin/tags/tags.page.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@
 import { FormsModule } from '@angular/forms'
 import { DialogModule, DialogService, TagListComponent, TagService } from '@platon/core/browser'
 import { Level, Topic } from '@platon/core/common'
+import { NzModalService } from 'ng-zorro-antd/modal'
 import { firstValueFrom } from 'rxjs'
 
 @Component({
@@ -20,7 +21,8 @@ export class AdminTagsPage implements OnInit {
   constructor(
     private readonly tagService: TagService,
     private readonly dialogService: DialogService,
-    private readonly changeDetectorRef: ChangeDetectorRef
+    private readonly changeDetectorRef: ChangeDetectorRef,
+    private readonly modal: NzModalService
   ) {}
 
   async ngOnInit(): Promise<void> {
@@ -50,19 +52,74 @@ export class AdminTagsPage implements OnInit {
     }
   }
 
+  private confirmAddTopic(sujet: string, name: string, originalName: string): Promise<'cancel' | 'force' | 'similar'> {
+    return new Promise((resolve) => {
+      const modalRef = this.modal.create({
+        nzTitle: `${sujet} similaire trouvé`,
+        nzContent: `Le ${sujet} "${name}" est similaire à un autre intitulé "${originalName}". Que voulez-vous faire ?`,
+        nzClosable: false,
+        nzCancelText: null,
+        nzOkText: null,
+        nzFooter: [
+          {
+            label: "Prendre l'intitulé similaire",
+            type: 'default',
+            onClick: () => {
+              modalRef.destroy()
+              resolve('similar')
+            },
+          },
+          {
+            label: "Forcer l'ajout",
+            type: 'primary',
+            danger: true,
+            onClick: () => {
+              modalRef.destroy()
+              resolve('force')
+            },
+          },
+          {
+            label: 'Annuler',
+            type: 'default',
+            onClick: () => {
+              modalRef.destroy()
+              resolve('cancel')
+            },
+          },
+        ],
+      })
+    })
+  }
+
   async createTag(name: string, type: 'topic' | 'level') {
     try {
       const tag = await firstValueFrom(
         type === 'level' ? this.tagService.createLevel({ name }) : this.tagService.createTopic({ name })
       )
       if (type === 'level') {
-        this.levels = [...this.levels, tag].sort((a, b) => a.name.localeCompare(b.name))
+        if (tag.existing) {
+          const action = await this.confirmAddTopic('Niveau', name, tag.name)
+          if (action === 'force') {
+            const forcedLevel = await firstValueFrom(this.tagService.createLevel({ name, force: true }))
+            this.levels = [...this.levels, forcedLevel].sort((a, b) => a.name.localeCompare(b.name))
+          }
+        } else {
+          this.levels = [...this.levels, tag].sort((a, b) => a.name.localeCompare(b.name))
+        }
       } else {
-        this.topics = [...this.topics, tag].sort((a, b) => a.name.localeCompare(b.name))
+        if (tag.existing) {
+          const action = await this.confirmAddTopic('Sujet', name, tag.name)
+          if (action === 'force') {
+            const forcedTopic = await firstValueFrom(this.tagService.createTopic({ name, force: true }))
+            this.topics = [...this.topics, forcedTopic].sort((a, b) => a.name.localeCompare(b.name))
+          }
+        } else {
+          this.topics = [...this.topics, tag].sort((a, b) => a.name.localeCompare(b.name))
+        }
       }
     } catch {
       this.dialogService.error(
-        'Une erreur est survenue lors de la suppression du tag, veuillez réessayer un peu plus tard !'
+        'Une erreur est survenue lors de la création du tag, veuillez réessayer un peu plus tard !'
       )
     } finally {
       this.changeDetectorRef.markForCheck()
