af07bb3c4b5260dd193697c41926c1a4c00235f7
Mamadou Cisse <mciissee@gmail.com>
2025-02-27 13:00:40 +0100

    feat(core-server): vector type support in typeorm

--- libs/core/server/src/lib/database/database.cli.ts
+++ libs/core/server/src/lib/database/database.cli.ts
@@ -6,3 +6,7 @@ export const AppDataSource = new DataSource({
   entities: ['libs/**/*.entity.ts', 'libs/**/*.view.ts'],
   migrations: ['migrations/*.ts'],
 } as DataSourceOptions)
+
+// @ts-expect-error TypeORM does not support but the database supports
+// https://github.com/typeorm/typeorm/issues/10056
+AppDataSource.driver.supportedDataTypes.push('vector')
