6fa8d928ba4a0e5511b30eafdb5191fcf692bda4
Mamadou Cisse <mciissee@gmail.com>
2025-03-01 17:20:47 +0100

    refactor(webcomponent): drop doc components

--- libs/feature/webcomponent/src/lib/docs/docs.module.ts
+++ libs/feature/webcomponent/src/lib/docs/docs.module.ts
@@ -2,25 +2,13 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { NgeMarkdownModule } from '@cisstech/nge/markdown'
-import { IDynamicModule } from '@cisstech/nge/services'
 import { NgJsonEditorModule } from 'ang-jsoneditor'
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip'
-import { DocsComponent } from './docs.component'
-import { JSONSchemaExpandablePipe } from './pipes/json-schema-expandable.pipe'
-import { JSONSchemaTypeNamePipe } from './pipes/json-schema-type-name.pipe'
-import { PropertiesComponent } from './properties/properties.component'
 import { ShowcaseComponent } from './showcase/showcase.component'
 
 @NgModule({
   imports: [CommonModule, MatButtonModule, NzToolTipModule, NgeMarkdownModule, NgJsonEditorModule],
-  declarations: [
-    DocsComponent,
-    JSONSchemaExpandablePipe,
-    JSONSchemaTypeNamePipe,
-    PropertiesComponent,
-    ShowcaseComponent,
-  ],
+  declarations: [ShowcaseComponent],
+  exports: [ShowcaseComponent],
 })
-export class DocsModule implements IDynamicModule {
-  component = DocsComponent
-}
+export class DocsModule {}
